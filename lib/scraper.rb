446ad1b71bc7f2455bd9e45ba38980fc70b17c8a
Charles Pelton <charliejp0311@gmail.com>
2020-02-26 17:36:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,35 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    page = open(index_url)
+    scrape = Nokogiri::HTML(page)
+    scrape.css('.student-card').each do |student|
+      students << {:location => student.css('.student-location').text,
+                    :name => student.css('h4.student-name').text,
+                    :profile_url => student.css('a').attribute('href').value
+                  }
+    end
+    students
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = {}
+    profile_page = open(profile_url)
+    profile_scrape = Nokogiri::HTML(profile_page)
+    profile = {
+      :linkedin => nil,
+      :github => nil,
+      :profile_quote => nil,
+      :bio => nil
+    }
+    binding.pry
+    profile
   end
 
 end
-
